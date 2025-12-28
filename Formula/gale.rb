f1b71cbfa605e7d1b7067210cd472a6dc7bc050c
Manash Kumar Mandal <manashmndl@gmail.com>
2025-12-28 12:50:49 +0100

    feat: update gale to v2025.1228.3

--- Formula/gale.rb
+++ Formula/gale.rb
@@ -3,27 +3,27 @@ require_relative "../lib/private_strategy"
 class Gale < Formula
   desc "Self-hosted GitHub Actions runner manager with auto-scaling"
   homepage "https://github.com/manashmandal/gale"
-  version "2025.1228.2"
+  version "2025.1228.3"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/manashmandal/gale/releases/download/v2025.1228.2/gale_2025.1228.2_darwin_amd64.tar.gz", using: GitHubPrivateRepositoryDownloadStrategy
+      url "https://github.com/manashmandal/gale/releases/download/v2025.1228.3/gale_2025.1228.3_darwin_amd64.tar.gz", using: GitHubPrivateRepositoryDownloadStrategy
       sha256 ""
     end
     on_arm do
-      url "https://github.com/manashmandal/gale/releases/download/v2025.1228.2/gale_2025.1228.2_darwin_arm64.tar.gz", using: GitHubPrivateRepositoryDownloadStrategy
+      url "https://github.com/manashmandal/gale/releases/download/v2025.1228.3/gale_2025.1228.3_darwin_arm64.tar.gz", using: GitHubPrivateRepositoryDownloadStrategy
       sha256 ""
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/manashmandal/gale/releases/download/v2025.1228.2/gale_2025.1228.2_linux_amd64.tar.gz", using: GitHubPrivateRepositoryDownloadStrategy
+      url "https://github.com/manashmandal/gale/releases/download/v2025.1228.3/gale_2025.1228.3_linux_amd64.tar.gz", using: GitHubPrivateRepositoryDownloadStrategy
       sha256 ""
     end
     on_arm do
-      url "https://github.com/manashmandal/gale/releases/download/v2025.1228.2/gale_2025.1228.2_linux_arm64.tar.gz", using: GitHubPrivateRepositoryDownloadStrategy
+      url "https://github.com/manashmandal/gale/releases/download/v2025.1228.3/gale_2025.1228.3_linux_arm64.tar.gz", using: GitHubPrivateRepositoryDownloadStrategy
       sha256 ""
     end
   end
